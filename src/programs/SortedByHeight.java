56234fc2036dc157ab46f9b0b99185cfccd5355b
pankaj1010 <pankajdevgade10@gmail.com>
2019-04-26 12:47:41 +0530

    Sortedbyheight

--- src/programs/SortedByHeight.java
+++ src/programs/SortedByHeight.java
@@ -1,10 +1,44 @@
 package programs;
 
+import java.util.Arrays;
+
 public class SortedByHeight {
+	public static int[] height(int[]a) {
+		int[] sort = new int[a.length];
+		int count =0;
+		for (int i = 0; i < sort.length; i++) {
+			if (a[i]==-1) {
+				sort[i]=a[i];
+				count++;
+			}
+		}
+		Arrays.sort(a);
+		if (count==0) {
+			return a;	
+		}
+		
+		int g;
+		for ( g = 0; g < sort.length; g++) {
+			if (a[g]!=-1) {
+				break;
+			}
+		}
+		for (int i = 0; i < sort.length; i++) {
+			if (sort[i]!=-1) {
+				sort[i]=a[g];
+				g++;
+			}
+		}
+		
+		return sort;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] a = {4, 2, 9, 11, 2, 16};
+		
+		for (int i : height(a)) {
+			System.out.print(i+" ");
+		}
 	}
 
 }
